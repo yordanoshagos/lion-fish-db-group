8a01fcca325d673caddae11a784f403f5cc97700
Danait Semere <semeregdanait@gmail.com>
2025-05-07 21:26:51 +0300

    added progress_table to malnutrition schema

--- progress_table.sql
+++ progress_table.sql
@@ -1,14 +1,14 @@
 create schema malnutrition;
-create table malnutrition.child_progress(
-progress_id VARCHR(10) primary key,
-child_id VARCHAR(10) references malnutrition.progress_table(child_id),
+create table malnutrition.progress_table(
+progress_id VARCHAR(10) primary key,
+child_id VARCHAR(10) references malnutrition.children_table(child_id),
 current_weight_in_kg numeric not null,
 current_height_in_cm numeric  not null,
 per_day_number_of_meals INT not null check (per_day_number_of_meals>=0),
 date_of_record DATE,
 );
 insert into 
-malnutrition.child_progress(progress_id,child_id,current_weight_in_kg,current_height_in_cm,per_day_number_of_meals,date_of_record);
+malnutrition.progress_table(progress_id,child_id,current_weight_in_kg,current_height_in_cm,per_day_number_of_meals,date_of_record);
 values 
 ('PO01','C001',17.0,102.1,4,'2025-01-20'),
 ('P002','C002',13.7,103.2,2,'2025-02-22'),
@@ -25,11 +25,12 @@ values
 ('P013','C013',15.3,89.4,3,'2025-11-20'),
 ('P014','C014',14.2,91.6,4,'2025-10-19'),
 ('P015','C015',12.5,88.5,3,'2025-11-20');
-select*from malnutrition.child_progress;
+select*from malnutrition.progress_table;
 select COUNT (*) 
 from (
 select child_id,
 MIN(current_weight_in_kg/POWER(current_height_in_cm/100.0,2) as maximum_bmi,
 MAX(current_weight_in_kg/POWER(current_height_in_cm/100.0,2) as maximum_bmi;
-from malnutrition.child_progress
-group by child_id as
+from malnutrition.progress_table
+group by child_id) as bmi_compared
+where maximum_bmi > minimum_bmi; 
